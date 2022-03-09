6c3e15b157299ad1c306c0ed0db683efa15fd276
Ajinkya Joshi <95956960+Ajinkya6898@users.noreply.github.com>
2022-03-09 20:27:17 +0530

    Assignment Complete

---
+++ Day-3/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.use(logger)
+
+app.get('/books',(req,res) =>{
+     res.send('Fetching all books');
+});
+
+app.get('/book/:name',(req,res)=>{
+     res.send({book1:"Game Of Thrones"})
+});
+
+function logger(req,res,next){
+     console.log('log');
+     next();
+}
+
+app.listen(2000);
